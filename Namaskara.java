b572ca356697c5f916846f32f7ca84ef8c3c394b
varsha2296 <72919418+varsha2296@users.noreply.github.com>
2020-10-20 14:25:47 +0530

    My First Commit

---
+++ Namaskara.java
@@ -0,0 +1,18 @@
+class Wish
+{
+//this is comments
+//to start a program we need a method(main)
+//main method is an entry point
+public static void main(String[] varsha)
+{
+System.out.println("Namaskara");
+}
+}
+class Camera
+{
+public static void main(String camera[])
+{
+System.out.println("Camera is Kodak");
+System.out.println("price is lot");
+}
+}
