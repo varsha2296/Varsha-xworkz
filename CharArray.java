2b4db9059c56a3aaa798f034f22afa4a3289a5d0
Varsha Hiremath <72919418+varsha2296@users.noreply.github.com>
2020-10-22 16:18:56 +0530

    Update CharArray.java

--- CharArray.java
+++ CharArray.java
@@ -3,17 +3,42 @@ public class CharArray{
 	{
 		System.out.println("invoked main method by JVM");
 		arrayOfLetters();
+		System.out.println("exiting main method by JVM");
 	}
 	public static void arrayOfLetters()
 	{
 		System.out.println("invoked arrayOfLetters");
 		
-		char[] letters={'A','B','C','D','E','F','G','H','I','J'};
+		char firstLetter='A';
+		char thirdLetter='C';
+		char fifthLetter='E';
+		char seventhLetter='G';
+		char ninthLetter='I';
+		char eleventhLetter='K';
+		char thirteenthLetter='M';
+		char fifteenthLetter='O';
+		char seventeenthLetter='Q';
+		char ninteenthLetter='S';
+		
+		char[] letters={firstLetter,thirdLetter,fifthLetter,seventhLetter,ninthLetter,
+		eleventhLetter,thirteenthLetter,fifteenthLetter,seventeenthLetter,ninteenthLetter};
 		
 		int sizeOfArray=letters.length;
 		System.out.println("Total elements: "+sizeOfArray);
 		
+		char elementAtIndex0=letters[0];
+		System.out.println("Element at index Zero: "+elementAtIndex0);
+		
+		char elementAtIndex2=letters[2];
+		System.out.println("Element at index two: "+elementAtIndex2);
+		
+		char elementAtIndex4=letters[4];
+		System.out.println("Element at index four: "+elementAtIndex4);
+		
 		char elementAtIndex6=letters[6];
 		System.out.println("Element at index six: "+elementAtIndex6);
+		
+		char elementAtIndex8=letters[8];
+		System.out.println("Element at index eight: "+elementAtIndex8);
 	}
 }
