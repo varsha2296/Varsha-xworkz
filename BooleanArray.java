95482e25591efd2b1d8a16c8b226952bd20b41ae
Varsha Hiremath <72919418+varsha2296@users.noreply.github.com>
2020-10-22 16:25:36 +0530

    Update BooleanArray.java

--- BooleanArray.java
+++ BooleanArray.java
@@ -3,17 +3,31 @@ public class BooleanArray{
 	{
 		System.out.println("invoked main method by JVM");
 		arrayOfBoolean();
+		System.out.println("exiting main method by JVM");
 	}
 	public static void arrayOfBoolean()
 	{
 		System.out.println("invoked arrayOfBoolean");
 		
-		boolean[] booleanValues={true,false};
+		boolean laptopHasAllApps=false;
+		final boolean laptopShowsTime=true;
+		boolean	cableHasAllChannels=true;
+		boolean	chocolateIsRed=false;
+		boolean leafyVegetablesAreGreen=true;
+		
+		boolean[] booleanValues={laptopHasAllApps,laptopShowsTime,
+		cableHasAllChannels,chocolateIsRed,leafyVegetablesAreGreen};
 		
 		int sizeOfArray=booleanValues.length;
 		System.out.println("Total elements: "+sizeOfArray);
 		
 		boolean elementAtIndex1=booleanValues[1];
 		System.out.println("Element at index one: "+elementAtIndex1);
+		
+		boolean elementAtIndex2=booleanValues[2];
+		System.out.println("Element at index two: "+elementAtIndex2);
+
+		boolean elementAtIndex3=booleanValues[3];
+		System.out.println("Element at index three: "+elementAtIndex3);
 	}
 }
