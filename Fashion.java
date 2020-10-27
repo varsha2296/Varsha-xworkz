953b1051dcd7092129014abbaf853de9927a9c61
Varsha Hiremath <72919418+varsha2296@users.noreply.github.com>
2020-10-27 14:50:48 +0530

    Update Fashion.java

--- Fashion.java
+++ Fashion.java
@@ -1,8 +1,9 @@
 public class Fashion{
-	public static void women(String clothes)
+	public static void women(String[] dresses)
 	{
 		System.out.println("invoked women from Fashion class");
 		
-		System.out.println("Element @ index 0 "+clothes);
+		System.out.println("Element @ index 0 "+dresses[0]);
+		System.out.println("Element @ index 1 "+dresses[1]);		
 	}
 }
