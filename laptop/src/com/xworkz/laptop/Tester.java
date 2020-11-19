a5f8225114159c01a4290a2407b52d76a4f21efb
Varsha Hiremath <72919418+varsha2296@users.noreply.github.com>
2020-11-19 21:56:25 +0530

    Packages and access specifiers

---
+++ laptop/src/com/xworkz/laptop/Tester.java
@@ -0,0 +1,16 @@
+package com.xworkz.laptop;
+
+import com.xworkz.laptop.company.Admin;
+import com.xworkz.laptop.company.Lenovo;
+
+public class Tester {
+
+	public static void main(String[] args) {
+
+		Admin.develop();
+
+		Lenovo.getModelNumber();
+
+	}
+
+}
